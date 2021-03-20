674591cbddf048a2256bd758606c8503a95060e8
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-03-21 00:18:24 +0530

    redirecting the user if not logged in

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3003;
+const middleware = require('./middleware');
 
 const server = app.listen(port, () => console.log('Server listening on port: ' + port));
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-app.get('/', (req, res, next) => {
-
+app.get('/', middleware.requireLogin, (req, res, next) => {
     const payload = {
-        pageTitle: 'Home'
+        pageTitle: 'Home',
     };
     res.status(200).render('home', payload);
 });
