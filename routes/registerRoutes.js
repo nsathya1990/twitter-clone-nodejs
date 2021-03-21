be45ffaac0f1a0c03c4e571a582a56f9602ca41f
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-03-21 13:23:12 +0530

    checking for empty fields

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -14,7 +14,22 @@ router.get('/', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
     console.log(req.body);
-    res.status(200).render('register');
+
+    const firstName = req.body.firstName.trim();
+    const lastName = req.body.lastName.trim();
+    const username = req.body.username.trim();
+    const email = req.body.email.trim();
+    const password = req.body.password;
+
+    const payload = req.body;
+
+    if (firstName && lastName && username && email && password) {
+        console.log('SUCCESS');
+    } else {
+        console.log('ERROR');
+        payload.errorMessage = 'Make sure each field has a valid value.';
+        res.status(200).render('register', payload);
+    }
 });
 
 module.exports = router;
