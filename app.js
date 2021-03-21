d690ddef1dc742f2036eed9b3a809e31d5f7eefc
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-03-21 20:51:46 +0530

    sessions

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const middleware = require('./middleware');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('./database');
+const session = require('express-session');
 
 const server = app.listen(port, () => console.log('Server listening on port: ' + port));
 
@@ -13,6 +14,14 @@ app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    session({
+        secret: 'potato chips',
+        resave: true,
+        saveUninitialized: false,
+    })
+);
 
 /** Routes */
 const loginRoutes = require('./routes/loginRoutes');
