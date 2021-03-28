f62f20d791851c2d5461508bc6425f9ef8332bc3
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-03-28 11:11:37 +0530

    enabling the submit button when the user types

--- public/js/common.js
+++ public/js/common.js
@@ -2,8 +2,19 @@
     // alert('spongebob');
 });
  */
-$('#postTextarea').keyup(event => {
+$('#postTextarea').keyup((event) => {
     const textbox = $(event.target);
     const value = textbox.val().trim();
     console.log(value);
+
+    const submitButton = $('#submitPostButton');
+    if (!submitButton.length) {
+        return alert('No submit button found');
+    }
+
+    if (!value) {
+        submitButton.prop('disabled', true);
+        return;
+    }
+    submitButton.prop('disabled', false);
 });
