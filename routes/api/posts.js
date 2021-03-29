4f0b934741a62676aaa95a1f09c7b5d9402a55e1
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-03-29 10:39:09 +0530

    ordering the posts by newest first

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -10,8 +10,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 router.get('/', (req, res, next) => {
     Post.find()
         .populate('postedBy')
+        .sort({ createdAt: -1 })
         .then((results) => res.status(200).send(results))
-        .catch(error => {
+        .catch((error) => {
             console.log(error);
             res.sendStatus(400);
         });
